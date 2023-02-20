3a03fd96b09146d1c055f8593b59e385fdb36d29
Felipe Murao <felipe.murao@gmail.com>
2023-02-20 10:23:49 -0300

    created some packages for project structure, created the App class, put some libraries implementation

---
+++ GitHubAPIProject/app/src/main/java/com/example/githubapiproject/presentation/GithubAPIApp.kt
@@ -0,0 +1,9 @@
+package com.example.githubapiproject.presentation
+
+import android.app.Application
+
+class GithubAPIApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
